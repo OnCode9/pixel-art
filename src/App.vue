3f0670f720573b351e1829a224b5308c0f0b615e
Dave Follett <davefollett@gmail.com>
2024-01-05 10:33:24 -0500

    Added supabase client library and was able to upsert to the database, rendered a grid, figured out vue 3 v-bind for css (its amazing).

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script setup>
+import { supabase } from './services/supabase'
+import Button from 'primevue/button'
 import HelloWorld from './components/HelloWorld.vue'
+import Cell from './components/Cell.vue'
+
+const numRows = 100
+const numCols = 100
+const size = '.5rem'
+const cssGridColumnTemplate = `repeat(${numCols}, ${size})`
+
+async function createCell() {
+  console.log('createCell() - start')
+
+const { data, error } = await supabase
+  .from('cells')
+  .upsert({ row: 0, col: 1, value: '#424242' })
+  .select()
+}
 </script>
 
 <template>
   <HelloWorld></HelloWorld>
+  <Button label="Create Cell" @click="createCell" />
+  <div class="grid">
+    <Cell v-for="index in 10000" color="#00FFFF" :size="size"/>
+  </div>
 </template>
 
 <style scoped>
-
+.grid {
+  display: grid;
+  grid-template-columns: v-bind(cssGridColumnTemplate);
+  outline: solid black;
+  width: fit-content;
+  height: fit-content;
+}
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  color: String,
+  size: String,
+})
+</script>
+
+<template>
+  <div class="cell"></div>
+</template>
+
+<style scoped>
+.cell {
+  background: v-bind(color);
+  width: v-bind(size);
+  height: v-bind(size);
+}
+.cell:hover {
+  background-color: grey;
+}
+</style>
