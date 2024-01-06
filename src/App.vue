b0a03b675795532beb2e2f2d27f1086cc0bd1dd8
Dave Follett <davefollett@gmail.com>
2024-01-06 00:17:07 -0500

    Worked out a lot of the interactions for changing the value of a cell.  Way harder than it should have been.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
 import { supabase } from './services/supabase'
 import Button from 'primevue/button'
-import HelloWorld from './components/HelloWorld.vue'
 import Cell from './components/Cell.vue'
 
-const numRows = 100
-const numCols = 100
+const numRows = 10
+const numCols = 10
 const size = '.5rem'
 const cssGridColumnTemplate = `repeat(${numCols}, ${size})`
 
@@ -20,10 +19,9 @@ const { data, error } = await supabase
 </script>
 
 <template>
-  <HelloWorld></HelloWorld>
   <Button label="Create Cell" @click="createCell" />
   <div class="grid">
-    <Cell v-for="index in 10000" color="#00FFFF" :size="size"/>
+    <Cell v-for="index in numCols * numRows" color="#FFFFFF" :size="size"/>
   </div>
 </template>
 
@@ -34,5 +32,6 @@ const { data, error } = await supabase
   outline: solid black;
   width: fit-content;
   height: fit-content;
+  margin-top: 1rem;
 }
 </style>
--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,19 +1,60 @@
 <script setup>
-defineProps({
+import { computed, ref } from "vue"
+import Dropdown from 'primevue/dropdown'
+import OverlayPanel from 'primevue/overlaypanel'
+
+const props = defineProps({
   color: String,
   size: String,
 })
+
+const validColors = ref([
+    { name: 'White', color: '#FFFFF' },
+    { name: 'Blue', color: '#0000FF' },
+    { name: 'Red', color: '#FF0000' },
+    { name: 'Green', color: '#00FF00' },
+    { name: 'Black', color: '#000000' }
+])
+
+const selectedColor = ref(validColors.value.find(item => item.color === props.color) || validColors.value[0])
+const op = ref();
+const cellBorder = ref('unset')
+
+const toggle = (event) => {
+  op.value.toggle(event);
+}
+
+const opClosed = () => {
+  console.log('op closed')
+  cellBorder.value = 'unset'
+}
+
+const opOpened = () => {
+  console.log('op opened')
+  cellBorder.value = 'solid grey'
+}
+
+const cellColor = computed(() => {
+  return selectedColor.value.color
+})
+
 </script>
 
 <template>
-  <div class="cell"></div>
+  <div class="cell" @click="toggle">
+    <OverlayPanel ref="op" showCloseIcon @show="opOpened" @hide="opClosed">
+      <p>{{ color }}</p>
+      <Dropdown v-model="selectedColor" :options="validColors" optionLabel="name" @change="toggle"/>
+    </OverlayPanel>
+  </div>
 </template>
 
 <style scoped>
 .cell {
-  background: v-bind(color);
+  background: v-bind(cellColor);
   width: v-bind(size);
   height: v-bind(size);
+  border: v-bind(cellBorder);
 }
 .cell:hover {
   background-color: grey;
